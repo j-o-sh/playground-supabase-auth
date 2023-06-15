664767ad611393cd846ca6296d3e169c00076ef6
Josh <ravens.aerie@googlemail.com>
2023-06-15 17:16:01 +0200

    redirect

--- auth.js
+++ auth.js
@@ -19,8 +19,7 @@ export default {
     const { data, error } =  await supabase.auth.signInWithOAuth({
       provider: 'github',
       options: {
-        // redirectTo: location.href
-        skipBrowserRedirect: true
+        redirectTo: location.href
       }
     })
     window.supa = supabase
